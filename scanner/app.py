d161896ee9ea73c770873057132be0be8548bb11
varun93 <1hvarun@gmail.com>
2019-04-29 15:35:06 -0400

    added support for network prefix and task splitting

--- scanner/app.py
+++ scanner/app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request
 from scanner import syn_scan,fyn_scan,grab_banner
 from query_results import get_results
 import json
+import ipaddress
 
 # initialize Flask
 flask_app = Flask(__name__)
@@ -16,6 +17,7 @@ def scan_results():
 	results = get_results()	
 	return json.dumps(results)
 
+# TODO : do input validation
 @flask_app.route('/submit_task', methods=['POST'])
 def submit_task():
 	dest_ip = request.form['ip_address'] 
@@ -23,5 +25,17 @@ def submit_task():
 	start_port = int(request.form['start_port']) 
 	end_port = int(request.form['end_port']) 
 	scan_mode = request.form['scan_mode'] 
-	grab_banner.delay('45.33.32.156', 80)
+	
+	if not network_prefix or not network_prefix.strip():
+		network_prefix = "32"
+
+	full_address = "{}/{}".format(dest_ip,network_prefix)
+	address_list = ipaddress.ip_network(full_address)
+
+	# not the most ideal way of splitting; can pool ips and ports; 
+	# but think about how to retrieve them while showing it to the user
+	for address in address_list:
+		for port in range(start_port, end_port + 1):
+			grab_banner.delay(str(address), port)
+
 	return json.dumps({"status" : "OK"})
